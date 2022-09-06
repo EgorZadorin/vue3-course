ff40184a9f6d690f706565cafe2480de0f56ff58
EgorZadorin <egozador@yandex.ru>
2022-09-06 22:34:31 +0300

    middle of the project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <h1>Page with posts</h1>
+    <div class="app_btns">
+      <my-button
+          @click="showDialog"
+      >
+        Create post
+      </my-button>
+      <my-select
+        v-model="selectedSort"
+        :options="sortOptions"
+      />
+    </div>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
+    <post-list
+        :posts="posts"
+        @remove="removePost"
+        v-if="!isPostsLoading"
+    />
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList"
+import MyDialog from "@/components/UI/MyDialog";
+import MyButton from "@/components/UI/MyButton";
+import axios from 'axios'
+import MySelect from "@/components/UI/MySelect";
+
+export default {
+  components: {MySelect, MyButton, MyDialog, PostForm, PostList},
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        {value: 'title', name: 'By name'},
+        {value: 'body', name: 'By description'},
+      ]
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+          this.posts = response.data
+          this.isPostsLoading = false;
+      } catch (e) {
+        alert('Error')
+      } finally {
+        this.isPostsLoading = false;
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts()
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.app_btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
 </style>
+
---
+++ src/components/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>List of posts</h3>
+    <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+    />
+  </div>
+  <h2 v-else style="color: red">
+    List of posts is empty
+  </h2>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
